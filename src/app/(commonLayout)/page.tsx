70de5c6e136a0ce7a716a690f630c222d142f4c6
Al Amin Sheikh <azalamin@MacBookPro.netis.cc>
2026-01-22 01:49:37 +0600

    feat(env) type safe for env data

--- src/app/(commonLayout)/page.tsx
+++ src/app/(commonLayout)/page.tsx
@@ -4,6 +4,7 @@ import { userService } from "@/services/user.service";
 export default async function Home() {
 	const session = await userService.getSession();
 	console.log(session);
+
 	return (
 		<div>
 			<Button variant='outline'>Click Me</Button>
