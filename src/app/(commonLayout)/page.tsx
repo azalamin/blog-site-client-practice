77596af5c3cf8c592dacaf1defd6a2ead18f92ae
Al Amin Sheikh <azalamin@MacBookPro.netis.cc>
2026-01-22 02:57:40 +0600

    feat(blog-card): load data of blog post

--- src/app/(commonLayout)/page.tsx
+++ src/app/(commonLayout)/page.tsx
@@ -3,7 +3,15 @@ import { blogService } from "@/services/blog.service";
 import { BlogPost } from "@/types";
 
 export default async function Home() {
-	const data = await blogService.getBlogPost();
+	const data = await blogService.getBlogPost(
+		{
+			isFeatured: true,
+			search: "post",
+		},
+		{
+			cache: "no-store",
+		},
+	);
 
 	return (
 		<div className='grid gap-6 sm:grid-cols-2 lg:grid-cols-3 px-20 pt-5'>
--- src/services/blog.service.ts
+++ src/services/blog.service.ts
@@ -1,11 +1,32 @@
 import { env } from "@/env";
+import { BlogServiceOptions, GetBlogsParams } from "@/types";
 
 const API_URL = env.API_URL;
 
 export const blogService = {
-	getBlogPost: async function () {
+	getBlogPost: async function (params: GetBlogsParams, options: BlogServiceOptions) {
 		try {
-			const res = await fetch(`${API_URL}/posts`);
+			const url = new URL(`${API_URL}/posts`);
+
+			if (params) {
+				Object.entries(params).forEach(([key, value]) => {
+					if (value !== undefined && value !== null && value !== "") {
+						url.searchParams.append(key, value);
+					}
+				});
+			}
+
+			const config: RequestInit = {};
+
+			if (options.cache) {
+				config.cache = options.cache;
+			}
+
+			if (options.revalidate) {
+				config.next = { revalidate: options.revalidate };
+			}
+
+			const res = await fetch(url.toString(), config);
 			const data = await res.json();
 
 			if (data.success) {
