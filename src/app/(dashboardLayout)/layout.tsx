8e2c2c3a993246dce4c3b9649443a8062e404cda
Al Amin Sheikh <azalamin@MacBookPro.netis.cc>
2026-01-27 14:03:17 +0600

    feat(layout): update the side bar

--- src/app/(dashboardLayout)/layout.tsx
+++ src/app/(dashboardLayout)/layout.tsx
@@ -1,5 +1,6 @@
 import { AppSidebar } from "@/components/layout/app-sidebar";
-import { SidebarInset, SidebarProvider } from "@/components/ui/sidebar";
+import { Separator } from "@/components/ui/separator";
+import { SidebarInset, SidebarProvider, SidebarTrigger } from "@/components/ui/sidebar";
 import { Roles } from "@/constants/roles";
 import { userService } from "@/services/user.service";
 
@@ -18,6 +19,10 @@ export default async function DashboardLayout({
 		<SidebarProvider>
 			<AppSidebar user={userInfo} />
 			<SidebarInset>
+				<header className='flex h-16 shrink-0 items-center gap-2 border-b px-4'>
+					<SidebarTrigger className='-ml-1' />
+					<Separator orientation='vertical' className='mr-2 data-[orientation=vertical]:h-4' />
+				</header>
 				<div className='flex flex-1 flex-col gap-4 p-4'>
 					{userInfo.role === Roles.ADMIN ? admin : user}
 				</div>
